cfa333a4d36ad1db4edc1c12ff2e3765dded5ff6
Austin Croucher <austin.croucher@gmail.com>
2023-05-18 12:44:12 -0400

    removed margin

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -10,7 +10,7 @@ function Card() {
                     <div className='card--top'>
                         <img className='subreddit--avatar' src={xboxLogo} alt="subreddit Avatar" />
                         <p className='subreddit--text'>r/Xboxone</p>
-                        <p className='postedBy--text'>Posed by u/Shotgun247 23 hours ago</p>
+                        <p className='postedBy--text'>Posted by u/Shotgun247 23 hours ago</p>
                     </div>
                     <button className='join--button'>Join</button>
 
